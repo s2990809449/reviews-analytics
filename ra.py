82372ac3fed0748ce48dab294c32cd00ff03a836
Tom Wang <s22659211@yahoo.com.tw>
2022-05-30 04:31:07 +0800

    增加留言平均長度

--- ra.py
+++ ra.py
@@ -6,8 +6,13 @@ with open('reviews.txt', 'r') as f:
 		count += 1 #和count = count + 1 相等
 		if count % 1000 == 0: #和1000求餘數=0
 			print(len(data)) #print 很花時間
-print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
-print(data[0])
-print('------------------')
-print(data[1])
+with open('reviews.txt', 'r') as a:
+	for line in a:
+		data.append(line)
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('每筆留言的平均長度:', sum_len/len(data))
